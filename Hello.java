5740d59cc057e537bfc1dd53548bd182f41e8029
AnushkaPathak2712 <anushkap.mca25@siescoms.sies.edu.in>
2026-03-08 23:24:34 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Updated code from GitHub");
+       System.out.println("Build triggered via ngrok webhook");
     }
 }
